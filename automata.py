c1a61c55c60c369122691fb8c9d0b75c21b79972
Simon Renblad <simon.renblad@outlook.com>
2021-10-17 02:13:15 +0800

    PDA process implementation

--- automata.py
+++ automata.py
@@ -38,13 +38,32 @@ class FiniteAutomaton(AbstractMachine):
 
 class PushdownAutomaton(AbstractMachine):
     """
+    states: list
+    transitions: dictionary of tuples in format (start_node, read, pop_stack) --> (end_node, push_stack)
+    starting_state: a value in states
+    accepting_states: a sublist of states
+    current_state: tracking variable
+    stack: a stack implemented as list
     
+    by default the PDA is input-driven, meaning it accepts by empty stack
     """
-    def __init__(self, states, transitions, starting_state, accepting_states):
-        super().__init__(states, transitions, starting_state, accepting_states)
+    def __init__(self, states, transitions, starting_state):
+        super().__init__(states, transitions, starting_state, None)
+        self.current_state = starting_state
+        self.stack = []
 
     def process(self,input) -> bool:
-        pass
+        """
+        input: list to be accepted by the PDA
+        """
+        for i in input:
+            out = self.transitions.get((self.current_state, i, (self.stack.pop() if self.stack else None)))
+            if out == None:
+                return False
+            self.current_state = out[0]
+            self.stack += out[1]
+
+        return (len(self.stack)==0) # input driven definition
 
     def visualize(self) -> None:
         pass
--- examples.py
+++ examples.py
@@ -1,5 +1,7 @@
 import automata 
 
+# ababab FA
+
 t = {
     (1,'a'): 2,
     (2, 'b'): 1
@@ -8,3 +10,16 @@ t = {
 fa = automata.FiniteAutomaton([1,2], t, 1, [1])
 
 print(fa.process("ababa"))
+
+# aaabbb PDA to check if N(a) = N(b)
+
+t2 = {
+    (1, 'a', 'x'): (1, ['x','x']),
+    (1, 'b', 'x'): (1, []),
+    (1, 'a', None): (1, ['x']),
+    (1, 'b', None): (2, [])
+}
+
+pda = automata.PushdownAutomaton([1,2], t2, 1)
+
+print(pda.process("aaaabb"))
